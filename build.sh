ddd39db8383c1429c596724819db35035a1883a1
Marco Tormento <marco.tormento@vipera.com>
2021-04-01 22:58:43 +0200

    Fixed build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-extldflags "-static"' go/charge_time.go
-mv charge_time bin
+CGO_ENABLED=0 GOOS=linux go build -o bin/evchargetime -a -installsuffix cgo -ldflags '-extldflags "-static"' go/charge_time.go
